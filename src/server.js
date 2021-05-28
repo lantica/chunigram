195f8cf38ef351cdd312163d0e956a232b1d5bd8
Sariel <lucifer0702@hotmail.com>
2021-05-29 00:21:34 +0800

    update log

--- src/server.js
+++ src/server.js
@@ -50,11 +50,11 @@
     // 1. add cache for update_id
     // 2. add /start description
     async function botHandler({ data: { update_id, message: { chat, text } } }) {
+        console.log("[tgBot] ", chat, text);
         if (chat.type !== "private") {
             sendMessage(chat.id, `Sorry. This bot does not work in ${chat.type}`);
             return json({});
         }
-        console.log("[tgBot] ", chat.id, text);
         switch (text) {
             case ("/start"): {
                 sendMessage(chat.id, "Please go to https://github.com/lantica/chunigram for usage");
